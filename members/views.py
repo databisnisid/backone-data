f0b4eaa2fca035abf8a6751f6dcfe46c55e88640
gkiserpong <gkiserpongit@gmail.com>
2024-02-29 06:14:15 +0700

    Online Status for MAP 5

--- members/views.py
+++ members/views.py
@@ -41,12 +41,13 @@ def prepare_data(members):
         member_geo['lng'] = lng
 
         # Online or Offline
-        offline_at = time.strptime(member.offline_at)
         member_geo['is_online'] = 0
         if member.offline_at is None:
             member_geo['is_online'] = 1
-        elif offline_at <= timezone.now():
-            member_geo['is_online'] = 1
+        else:
+            offline_at = time.strptime(member.offline_at)
+            if offline_at <= timezone.now():
+                member_geo['is_online'] = 1
 
         new_members.append(member_geo)
 
