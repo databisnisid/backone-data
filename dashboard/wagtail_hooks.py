0ff9042d2a34f1878637b0a98ad3c7d7829d9c48
proitlab <dedy.sutanto@proit.co.id>
2024-10-15 15:21:10 +0700

    Remove panels for group Quota Starlink

--- dashboard/wagtail_hooks.py
+++ dashboard/wagtail_hooks.py
@@ -1,52 +1,55 @@
-#from django.http import HttpRequest
-#from django.utils.safestring import mark_safe
-#from wagtail.admin.ui.components import Component
+# from django.http import HttpRequest
+# from django.utils.safestring import mark_safe
+# from wagtail.admin.ui.components import Component
 from django.core.exceptions import ObjectDoesNotExist
 from wagtail import hooks
 
 from dashboard.summary_panels import NetworksPanelSummary
-#from networks.models import Networks, NetworkRoutes
-#from members.models import Members
-#from crum import get_current_user
-#from wagtail.contrib.modeladmin.views import CreateView, EditView
+
+# from networks.models import Networks, NetworkRoutes
+# from members.models import Members
+# from crum import get_current_user
+# from wagtail.contrib.modeladmin.views import CreateView, EditView
 from .summary_panels import *
-#from django.utils.html import format_html
+
+# from django.utils.html import format_html
 from django.utils.translation import gettext as _
 from django.contrib.auth.models import Group
-#from axes.models import AccessAttempt, AccessLog, AccessFailureLog
-#from wagtail.snippets.models import register_snippet
-#from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
-#from django.conf import settings
+
+# from axes.models import AccessAttempt, AccessLog, AccessFailureLog
+# from wagtail.snippets.models import register_snippet
+# from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
+# from django.conf import settings
 
 
-@hooks.register('construct_reports_menu', order=1)
+@hooks.register("construct_reports_menu", order=1)
 def hide_reports_menu_item(request, menu_items):
-    menu_items[:] = [item for item in menu_items if item.name != 'workflows']
-    menu_items[:] = [item for item in menu_items if item.name != 'workflow-tasks']
-    menu_items[:] = [item for item in menu_items if item.name != 'aging-pages']
-    menu_items[:] = [item for item in menu_items if item.name != 'locked-pages']
+    menu_items[:] = [item for item in menu_items if item.name != "workflows"]
+    menu_items[:] = [item for item in menu_items if item.name != "workflow-tasks"]
+    menu_items[:] = [item for item in menu_items if item.name != "aging-pages"]
+    menu_items[:] = [item for item in menu_items if item.name != "locked-pages"]
 
 
-@hooks.register('construct_main_menu', order=2)
+@hooks.register("construct_main_menu", order=2)
 def hide_snippets_menu_item(request, menu_items):
-    menu_items[:] = [item for item in menu_items if item.name != 'documents']
-    menu_items[:] = [item for item in menu_items if item.name != 'explorer']
-    menu_items[:] = [item for item in menu_items if item.name != 'images']
-    menu_items[:] = [item for item in menu_items if item.name != 'help']
+    menu_items[:] = [item for item in menu_items if item.name != "documents"]
+    menu_items[:] = [item for item in menu_items if item.name != "explorer"]
+    menu_items[:] = [item for item in menu_items if item.name != "images"]
+    menu_items[:] = [item for item in menu_items if item.name != "help"]
 
     try:
-        group_support = Group.objects.get(name='Support')
+        group_support = Group.objects.get(name="Support")
     except ObjectDoesNotExist:
         group_support = []
 
-    #if not request.user.is_superuser or request.user.group not in group_support:
+    # if not request.user.is_superuser or request.user.group not in group_support:
     if not (request.user.is_superuser or group_support in request.user.groups.all()):
-        menu_items[:] = [item for item in menu_items if item.name != 'networks']
+        menu_items[:] = [item for item in menu_items if item.name != "networks"]
 
-    #if not group_support in request.user.groups.all():
+    # if not group_support in request.user.groups.all():
     #    menu_items[:] = [item for item in menu_items if item.name != 'networks']
 
-    '''
+    """
     if not request.user.is_superuser:
         if not request.user.organization.features.network_rules:
             menu_items[:] = [item for item in menu_items if item.name != 'network-rules']
@@ -63,7 +66,7 @@ def hide_snippets_menu_item(request, menu_items):
 
     if settings.HEADSCALE_ON == 0:
         menu_items[:] = [item for item in menu_items if item.name != 'backone-hs']
-    '''
+    """
 
 
 @hooks.register("construct_settings_menu", order=3)
@@ -74,18 +77,25 @@ def hide_user_menu_item(request, menu_items):
     menu_items[:] = [item for item in menu_items if item.name != "sites"]
     menu_items[:] = [item for item in menu_items if item.name != "collections"]
 
-@hooks.register('construct_homepage_panels', order=4)
+
+@hooks.register("construct_homepage_panels", order=4)
 def add_another_welcome_panel(request, panels):
     panels[:] = [panel for panel in panels if panel.name != "site_summary"]
-    panels[:] = [panel for panel in panels if panel.name != "workflow_pages_to_moderate"]
+    panels[:] = [
+        panel for panel in panels if panel.name != "workflow_pages_to_moderate"
+    ]
     panels[:] = [panel for panel in panels if panel.name != "pages_for_moderation"]
-    panels[:] = [panel for panel in panels if panel.name != "user_pages_in_workflow_moderation"]
+    panels[:] = [
+        panel for panel in panels if panel.name != "user_pages_in_workflow_moderation"
+    ]
     panels[:] = [panel for panel in panels if panel.name != "locked_pages"]
 
-    panels.append(MapSummaryPanel())
-    panels.append(NetworksPanelSummary())
+    if request.user.groups.all()[0].name != "Quota Starlink":
+        panels.append(MapSummaryPanel())
+        panels.append(NetworksPanelSummary())
 
-'''
+
+"""
 
     #panels.append(NetworksSummaryPanel())
     #panels.append(MembersProblemPanel())
@@ -96,7 +106,7 @@ def add_another_welcome_panel(request, panels):
     if request.user.organization.features.number_of_network > 1:
         panels.append(NetworksChartsPanel())
 
-'''
+"""
 
 '''
 @hooks.register("insert_global_admin_js", order=100)
@@ -107,7 +117,7 @@ def global_admin_js():
     )
 '''
 
-'''
+"""
 class AccessAttemptSVS(SnippetViewSet):
     model = AccessAttempt
     menu_label = _('Access Attempt')
@@ -215,4 +225,4 @@ class AccessSnippetGroup(SnippetViewSetGroup):
 
 register_snippet(AccessSnippetGroup)
 
-'''
+"""
