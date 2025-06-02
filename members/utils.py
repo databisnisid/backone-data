b6e3924539c3df794f495af7a0a4610d49cb01fc
proitlab <dedy.sutanto@proit.co.id>
2025-06-02 16:40:02 +0700

    export EXCEL

--- members/utils.py
+++ members/utils.py
@@ -12,32 +12,34 @@ def get_members_by_net(domain_api, network_id):
         response_json = {}
 
         try:
-            #response = requests.get(domain_api + '/api/members/get_by_net/' + network_id + '/')
-            response = requests.get(domain_api + '/api/members/get_by_net_mqtt/' + network_id + '/')
+            # response = requests.get(domain_api + '/api/members/get_by_net/' + network_id + '/')
+            response = requests.get(
+                domain_api + "/api/members/get_by_net_mqtt/" + network_id + "/"
+            )
             response_json = response.json()
 
-            #print(response_json)
+            # print(response_json)
 
         except requests.exceptions.HTTPError as e:
             print(e.response.text)
 
         for resp_json in response_json:
-            member_name=resp_json['name']
-            member_code=resp_json['member_code']
-            member_description=resp_json['description']
-            member_id=resp_json['member_id']
-            member_address=resp_json['address']
-            member_location=resp_json['location']
-            member_online_at=resp_json['online_at']
-            member_offline_at=resp_json['offline_at']
-            member_service_line=resp_json['mobile_number_first']
+            member_name = resp_json["name"]
+            member_code = resp_json["member_code"]
+            member_description = resp_json["description"]
+            member_id = resp_json["member_id"]
+            member_address = resp_json["address"]
+            member_location = resp_json["location"]
+            member_online_at = resp_json["online_at"]
+            member_offline_at = resp_json["offline_at"]
+            member_service_line = resp_json["mobile_number_first"]
 
             try:
-                member_quota_string=resp_json['mqtt']['quota_first']
+                member_quota_string = resp_json["mqtt"]["quota_first"]
             except TypeError:
-                member_quota_string=''
+                member_quota_string = ""
 
-            '''
+            """
             member_name=resp_json['fields']['name']
             member_code=resp_json['fields']['member_code']
             member_description=resp_json['fields']['description']
@@ -47,8 +49,7 @@ def get_members_by_net(domain_api, network_id):
             member_online_at=resp_json['fields']['online_at']
             member_offline_at=resp_json['fields']['offline_at']
             member_service_line=resp_json['fields']['mobile_number_first']
-            '''
-
+            """
 
             try:
                 member = Members.objects.get(member_id=member_id)
@@ -78,7 +79,5 @@ def get_members_all(domain_api):
     networks = Networks.objects.all()
 
     for network in networks:
-        print('NetworkID: ', network.network_id)
+        print("NetworkID: ", network.network_id)
         get_members_by_net(domain_api, network.network_id)
-
-
--- members/wagtail_hooks.py
+++ members/wagtail_hooks.py
@@ -1,8 +1,18 @@
 from crum import get_current_user
 from django.core.exceptions import ObjectDoesNotExist
 from wagtail.contrib.modeladmin.options import (
-    ModelAdmin, ModelAdminGroup, PermissionHelper, modeladmin_register)
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel, FieldRowPanel, InlinePanel, ObjectList
+    ModelAdmin,
+    ModelAdminGroup,
+    PermissionHelper,
+    modeladmin_register,
+)
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    ObjectList,
+)
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from wagtailgeowidget import geocoders
@@ -12,12 +22,13 @@ from .models import Members
 
 
 class MembersPermissionHelper(PermissionHelper):
-    '''
+    """
 
     def user_can_list(self, user):
         return True
 
-    '''
+    """
+
     def user_can_create(self, user):
         return False
 
@@ -27,28 +38,42 @@ class MembersPermissionHelper(PermissionHelper):
         else:
             return False
 
-    '''
+    """
     def user_can_edit_obj(self, user, obj):
         return False
-    '''
+    """
 
 
 class MembersAdmin(ModelAdmin):
     model = Members
-    menu_label = 'Sites'
-    menu_icon = 'globe'
+    menu_label = "Sites"
+    menu_icon = "globe"
     inspect_view_enabled = True
     add_to_settings_menu = False
     exclude_from_explorer = False
-    list_display = ('name_with_network', 'address_multiline', 'online_at', 'get_links', 'upload_baa')
-    search_fields = ('name', 'network__name', 'member_id', 
-                     'notes', 'invoice_number', 'quota_string',
-                     'network__network_group__name', 'service_line')
-    #list_filter = ('network',)
+    list_display = (
+        "name_with_network",
+        "address_multiline",
+        "online_at",
+        "get_links",
+        "upload_baa",
+    )
+    list_export = list_display
+    search_fields = (
+        "name",
+        "network__name",
+        "member_id",
+        "notes",
+        "invoice_number",
+        "quota_string",
+        "network__network_group__name",
+        "service_line",
+    )
+    # list_filter = ('network',)
     list_per_page = 100
     permission_helper_class = MembersPermissionHelper
 
-    '''
+    """
     panels = [
             MultiFieldPanel([
                 FieldPanel('name', read_only=True),
@@ -72,63 +97,86 @@ class MembersAdmin(ModelAdmin):
                 ]),
             FieldPanel('notes'),
             ]
-    '''
+    """
 
     def get_edit_handler(self):
         basic_panels = [
-                FieldPanel('name', read_only=True),
-                FieldPanel('network', read_only=True),
-                FieldPanel('address', read_only=True),
-                FieldRowPanel([
-                    FieldPanel('online_at', read_only=True),
-                    FieldPanel('offline_at', read_only=True),
-                ])
+            FieldPanel("name", read_only=True),
+            FieldPanel("network", read_only=True),
+            FieldPanel("address", read_only=True),
+            FieldRowPanel(
+                [
+                    FieldPanel("online_at", read_only=True),
+                    FieldPanel("offline_at", read_only=True),
                 ]
+            ),
+        ]
 
-        sales_panels = MultiFieldPanel([
-                FieldRowPanel([
-                    FieldPanel('links'),
-                    MultiFieldPanel([
-                        FieldPanel('upload_baa'),
-                        FieldPanel('invoice_number', read_only=True),
-                        ])
-                    ]),
-                FieldPanel('notes')])
+        sales_panels = MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("links"),
+                        MultiFieldPanel(
+                            [
+                                FieldPanel("upload_baa"),
+                                FieldPanel("invoice_number", read_only=True),
+                            ]
+                        ),
+                    ]
+                ),
+                FieldPanel("notes"),
+            ]
+        )
 
-        finance_panels = MultiFieldPanel([
-                FieldRowPanel([
-                    FieldPanel('links', read_only=True),
-                    MultiFieldPanel([
-                        FieldPanel('upload_baa', read_only=True),
-                        FieldPanel('invoice_number'),
-                        ])
-                    ]),
-                FieldPanel('notes', read_only=True)])
+        finance_panels = MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("links", read_only=True),
+                        MultiFieldPanel(
+                            [
+                                FieldPanel("upload_baa", read_only=True),
+                                FieldPanel("invoice_number"),
+                            ]
+                        ),
+                    ]
+                ),
+                FieldPanel("notes", read_only=True),
+            ]
+        )
 
-        support_panels = MultiFieldPanel([
-                FieldRowPanel([
-                    FieldPanel('links', read_only=True),
-                    MultiFieldPanel([
-                        FieldPanel('upload_baa', read_only=True),
-                        FieldPanel('invoice_number', read_only=True),
-                        ])
-                    ]),
-                FieldPanel('notes', read_only=True)])
+        support_panels = MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("links", read_only=True),
+                        MultiFieldPanel(
+                            [
+                                FieldPanel("upload_baa", read_only=True),
+                                FieldPanel("invoice_number", read_only=True),
+                            ]
+                        ),
+                    ]
+                ),
+                FieldPanel("notes", read_only=True),
+            ]
+        )
 
         user = get_current_user()
 
         try:
-            group_support = Group.objects.get(name='Support')
+            group_support = Group.objects.get(name="Support")
         except ObjectDoesNotExist:
             group_support = []
 
         try:
-            group_sales = Group.objects.get(name='Sales')
+            group_sales = Group.objects.get(name="Sales")
         except ObjectDoesNotExist:
             group_sales = []
 
         try:
-            group_finance = Group.objects.get(name='Finance')
+            group_finance = Group.objects.get(name="Finance")
         except ObjectDoesNotExist:
             group_finance = []
 
@@ -144,24 +192,44 @@ class MembersAdmin(ModelAdmin):
 
         return ObjectList(custom_panels)
 
-
     def get_list_display(self, request):
         if request.user.is_superuser:
-            list_display = ('name_with_parameters', 'address_multiline', 'network_group', 'online_at', 'offline_at', 'baa_file', 'invoice_number', 'notes')
+            list_display = (
+                "name_with_parameters",
+                "address_multiline",
+                "network_group",
+                "online_at",
+                "offline_at",
+                "baa_file",
+                "invoice_number",
+                "notes",
+            )
         else:
-            list_display = ('name_with_parameters', 'address_multiline', 'network_group', 'online_at', 'baa_file', 'invoice_number', 'notes')
-         
+            list_display = (
+                "name_with_parameters",
+                "address_multiline",
+                "network_group",
+                "online_at",
+                "baa_file",
+                "invoice_number",
+                "notes",
+            )
+
         return list_display
 
     def get_queryset(self, request):
-        #qs = Members.objects.none()
+        # qs = Members.objects.none()
         if request.user.is_superuser:
             qs = Members.objects.all()
         else:
             networks = request.user.organization.networks.all()
-            qs = Members.objects.filter(network__in=networks, offline_at__isnull=True) | Members.objects.filter(network__in=networks, offline_at__gt=timezone.now())
+            qs = Members.objects.filter(
+                network__in=networks, offline_at__isnull=True
+            ) | Members.objects.filter(
+                network__in=networks, offline_at__gt=timezone.now()
+            )
 
         return qs
 
-modeladmin_register(MembersAdmin)
 
+modeladmin_register(MembersAdmin)
